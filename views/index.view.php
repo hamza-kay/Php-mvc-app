461f7263ca3dd83fdc890e654a9bad4a7848405d
= <=>
2018-11-15 22:37:02 +0000

    DB tasks

--- views/index.view.php
+++ views/index.view.php
@@ -13,6 +13,16 @@
 </head>
 
 <body>
-<script src="js/scripts.js"></script>
+<h1>My Tasks</h1>
+
+<?php foreach ($tasks as $task) : ?>
+    <li>
+        <?php if ($task->completed) : ?>
+            <strike><?= $task->description; ?></strike>
+        <?php else : ?>
+            <?= $task->description; ?>
+        <?php endif; ?>
+    </li>
+<?php endforeach; ?>
 </body>
 </html>
